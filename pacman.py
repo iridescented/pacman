217d2ae0c5eccb071a7b3767d96123b3ae95f9b9
Leeland RajKumar <iridescentedmc@gmail.com>
2022-06-09 15:07:40 +0800

    8-6-22-1

---
+++ pacman.py
@@ -0,0 +1,102 @@
+#GOAL FOR TODAY
+#-Working UI
+
+
+
+#IMPORTS
+from http.client import REQUEST_URI_TOO_LONG
+import pygame
+import math
+import random
+
+#DEFINITIONS
+BLACK = (0, 0, 0)
+WHITE = (200, 200, 200)
+LGRAY = (211,211,211)
+GREEN = (0,255,0)
+RED   = (255,0,0)
+TILESIZE = 32
+WINDOW_WIDTH = 28*TILESIZE
+WINDOW_HEIGHT = 31*TILESIZE
+
+pacmanspawn = [14,17]
+#gameboard size = 28*31
+gameboard = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+    [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,1,1,1,1,1,1,1,1,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
+    [0,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,0],
+    [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+    [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
+    [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
+    [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+    [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
+    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
+
+
+#FUNCTIONS
+def drawGrid():
+    OVERLAY = pygame.Surface((TILESIZE, TILESIZE), pygame.SRCALPHA)
+    for x in range(0, WINDOW_WIDTH, TILESIZE):
+        for y in range(0, WINDOW_HEIGHT, TILESIZE):
+            match gameboard[math.floor(y/TILESIZE)][math.floor(x/TILESIZE)]:
+                case 1:
+                    OVERLAY.set_alpha(50)
+                    OVERLAY.fill(GREEN)
+                    print(str(math.floor(x/TILESIZE))+' '+str(math.floor(y/TILESIZE))+':'+str(gameboard[math.floor(y/WINDOW_WIDTH)][math.floor(x/WINDOW_HEIGHT)]))
+                    print("GAE")
+                case default:
+                    OVERLAY.set_alpha(0)
+                    OVERLAY.fill(RED)
+            SCREEN.blit(OVERLAY,(x,y))
+
+def start():
+    global SCREEN, OVERLAY, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('PACMAN')
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+    map = pygame.transform.scale(pygame.image.load('Assets/maze.png'), (WINDOW_WIDTH,WINDOW_HEIGHT))
+    SCREEN.blit(map,(0,0))
+    drawGrid()
+    print("Start Sequence")
+
+def update():
+    # print("Update Sequence")
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+    pygame.display.update()
+
+#MAIN
+def main():
+    print("Main Sequence")
+    start()
+    while True:
+        update()
+
+
+main()
